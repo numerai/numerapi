78bf4f270a6bb4005d80c107bd3e0316a4b0bfc3
Noah Harasz <noah@numer.ai>
2026-08-05 13:15:36 -0700

    update version tag to main version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def load(path):
     return open(path, "r").read()
 
 
-numerapi_version = "2.24.0.dev1"
+numerapi_version = "2.24.0"
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
